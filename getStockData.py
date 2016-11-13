87b4b4c1886190b573b3dd621ab1d0e7c7d8345b
Lang Tran <lang.tran96@gmail.com>
2016-11-12 19:51:12 -0800

    getStockData now accesses nasdaq api and downloads data for stocks as .xml files

---
+++ getStockData.py
@@ -0,0 +1,25 @@
+import io
+import requests
+
+token = '25D26255F6924F31BD86503A4253BEA0'
+start = '9/1/2015'
+end = '9/18/2015'
+arr = ['GOOG', 'APPL']
+
+def queryStock(stockArray, startDate, endDate):
+    result = [];
+    print(stockArray)
+    for stock in stockArray:
+        print(stock)
+        url='http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetEndOfDayData'
+        data = {'_Token' : '%s' % token,
+        'Symbols' : '%s' % stock,
+        'StartDate' : '%s' % startDate,
+        'EndDate' : '%s' % endDate,
+        'MarketCenters' : '' }
+        r = requests.get(url, params = data)
+        with io.FileIO(stock + ".xml", 'w') as f:
+            f.write(r.text)
+            f.close()
+
+queryStock(arr, start, end)
